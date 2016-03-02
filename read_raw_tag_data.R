28b34dc7b79d8de340357a826f66a5db9c531faf
Tom Evans <thomas.jude.evans@gmail.com>
2016-03-02 16:14:19 +0100

    Script to take in the raw tag data from the TDRs and output to a new table

---
+++ read_raw_tag_data.R
@@ -0,0 +1,16 @@
+# Script to process the raw tag data files then output to a new DB table.
+
+
+# First get deployment data from DB
+
+
+# For each tag, read in data
+
+# Convert to data frame
+
+# Add device_info_serial, file_name, and ring_number columns
+
+# Maybe also somthing to say whether it's during the device deployment?
+
+
+# Combine above and output to new table
