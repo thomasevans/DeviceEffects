e788c92f5bd5d6bfe075bc3cb7394c369894f27c
thomasevans <thomas.jude.evans@gmail.com>
2016-03-24 14:32:25 +0100

    Script to summarise dive paramaters for each deployment - use to make new plots

---
+++ dive_paramaters_summary.R
@@ -0,0 +1,45 @@
+# Calculate summary statistics for diving paramaters for each deployment
+
+
+# Load in deployment data ----
+
+
+
+# Load in diving data ----
+
+
+
+# Load in dive bout data ----
+
+
+
+# **** Make a summary table ----
+
+# First assemble deployment columns ---
+
+
+# Calculate summaries for various variables -----
+# For each get:
+# - mean
+# - median
+# - 95% CI
+# - SD
+
+
+# Number of dives per bout
+
+
+# Dive depth (maximum)
+
+
+# Descent and Ascent rates
+
+
+# Proportion of bottom time during dives
+
+
+# Output this to new DB table and save R binnary object ----
+
+
+
+# Next write plotting script (ggplots of with/ without GPS for different variables)
