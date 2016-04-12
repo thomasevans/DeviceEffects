4fcc65ee6861c2c33ed9faca998d6837f63fcd2d
Tom Evans <thomas.jude.evans@gmail.com>
2016-04-12 09:13:15 +0200

    Initialise new script to analyse PDI data

---
+++ pdi_analysis.R
@@ -0,0 +1,33 @@
+# Analyse PDI - post-dive interval
+
+# Load data ------
+
+# Load in dive data
+
+
+# Load in deployment information
+# (need to label by device order and device type deployed)
+
+
+# Look at PDI and dive duration distributions ----
+# Histograms of each
+
+# Calculate 0.05, 0.95 % quantiles for dive duration
+
+
+# Make PDI calculations -----
+
+# For each deployment do:
+
+# For intervals (20 s?) 100 - 250 s?? calculate:
+#' PDI min
+#' PDI 0.05 quantile
+#' PDI 0.10 quantile
+#' PDI 0.50 quantile (== median)
+#' PDI mean
+
+# Combine above into a single DF together with deployment information
+
+
+
+
