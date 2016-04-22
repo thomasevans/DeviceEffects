6a157ffb4a52e0ebc76a71adfee1517628bf891c
Tom Evans <thomas.jude.evans@gmail.com>
2016-04-22 10:32:15 +0200

    glmm etc.

--- pdi_analysis.R
+++ pdi_analysis.R
@@ -62,7 +62,7 @@ n <- length(deployments)
 # Initialise vectors
 
 dep.id <- duration.min <- duration.max <- pdi.min <- pdi_05 <- pdi_10 <- pdi_median <-
-  pdi_mean <- pdi_95 <- duration.mid <- n_dives <- NULL
+  pdi_mean <- pdi_95 <- pdi_25 <-  duration.mid <- n_dives <- NULL
 
 # i <- 1
 
@@ -90,13 +90,16 @@ for(i in 1:n){
     #' PDI min
     pdi.min[x] <- min(pdi_sub, na.rm = TRUE)
     pdi_quants <- NA
-    pdi_quants <- quantile(pdi_sub, c(0.05, 0.10, 0.50, 0.95), na.rm = TRUE)
+    pdi_quants <- quantile(pdi_sub, c(0.05, 0.10, 0.50, 0.95, 0.25), na.rm = TRUE)
 
     #' PDI 0.05 quantile
     pdi_05[x] <- pdi_quants[1]
     
     #' PDI 0.10 quantile
     pdi_10[x] <- pdi_quants[2]
+    
+    #' PDI 0.25 quantile
+    pdi_25[x] <- pdi_quants[5]
     
     #' PDI 0.50 quantile (== median)
     pdi_median[x] <- pdi_quants[3]
@@ -126,7 +129,7 @@ plot(pdi.min[nf]~duration.mid[nf], ylim = c(0,100), col = as.factor(dep.id))
 
 # Combine above into a single DF together with deployment information
 pdi_df <- cbind.data.frame(dep.id , duration.min , duration.max , pdi.min ,
-                          pdi_05 , pdi_10 , pdi_median, pdi_mean , pdi_95 ,
+                          pdi_05 , pdi_10 , pdi_25, pdi_median, pdi_mean , pdi_95 ,
                           duration.mid , n_dives)
 
 # Add deployment info to pdi df (GPS/ TDR and order of deployment) ----
@@ -153,16 +156,22 @@ library(scales)
 
 # Remove those points where values are based on few dives
 # hist(pdi_df_comb$n_dives, breaks = 40, xlim = (c(0,100)))
+
+pdi_df_comb$pdi_25_log <- log(pdi_df_comb$pdi_25)
+
+
 pdi_df_comb_sample_10 <- pdi_df_comb
 pdi_df_comb_sample_10[pdi_df_comb$n_dives < 10 ,c(4:7)] <- NA
 
+# pdi_df_comb_sample_10$pdi_25_log <- log(pdi_df_comb_sample_10$pdi_25)
 
-p <- ggplot(pdi_df_comb_sample_10, aes(duration.mid, pdi_05, col = factor(ring_number),
+pdi_df_comb_sample_10$duration.mid_new <- (pdi_df_comb_sample_10$duration.mid + 2.25 -(.5*as.numeric(as.factor(pdi_df_comb_sample_10$ring_number))))
+p <- ggplot(pdi_df_comb_sample_10, aes(duration.mid_new, pdi_25, col = factor(ring_number),
                              shape= as.factor(type))) +
   geom_line(aes(lty =  as.factor(type)), lwd = 1.5, alpha = 0.3,
-            position = position_jitterdodge(dodge.width = 5), show.legend = TRUE)+
+             show.legend = TRUE)+
   geom_point(alpha=0.6,
-             size=3, position = position_jitterdodge(dodge.width = 5)) 
+             size=3) 
 p <- p + theme_bw()
 # p + ylim(0,120)
 p
@@ -177,3 +186,68 @@ p
 # Analyse data with model form: 
 # PDI ~ dive_duration + treatment + 1|order/ID
 
+library(lme4)
+
+mod.1 <- glmer(pdi_25_log ~ duration.mid + type + (1|GPS_TDR_order/ring_number),
+               data = pdi_df_comb_sample_10)
+summary(mod.1)
+
+mod.1
+anova(mod.1)
+
+drop1(mod.1, test = "Chisq")
+
+plot(mod.1)
+str(mod.1)
+
+residuals(mod.1) < 1
+qqnorm(resid(mod.1))
+
+
+pdi_df_comb_sample_10$all_predict_25 <- predict(mod.1,pdi_df_comb_sample_10)
+resid_calc <- pdi_df_comb_sample_10$all_predict_25 - pdi_df_comb_sample_10$pdi_25_log
+plot(resid_calc)
+pdi_df_comb_sample_10_no_resid <- pdi_df_comb_sample_10[resid_calc > -2,]
+mod.1.no_resid <- glmer(pdi_25_log ~ duration.mid + type + (1|GPS_TDR_order/ring_number),
+               data = pdi_df_comb_sample_10_no_resid)
+summary(mod.1.no_resid)
+
+mod.1.no_resid
+anova(mod.1.no_resid)
+
+drop1(mod.1.no_resid, test = "Chisq")
+plot(mod.1.no_resid)
+
+pdi_df_comb_sample_10$all_predict_25_no_resid <- predict(mod.1.no_resid,pdi_df_comb_sample_10)
+
+
+# newdata <- with(pdi_df_comb_sample_10, expand.grid(type=unique(type), duration.mid = unique(duration.mid)))
+p2 <- predict(mod.1,pdi_df_comb_sample_10,re.form=NA)
+pdi_df_comb_sample_10$mod.1_pred <- p2
+pdi_df_comb_sample_10_pred <- unique(pdi_df_comb_sample_10[,c("duration.mid",
+                            "type", "mod.1_pred")])
+pdi_df_comb_sample_10_pred$mod.1_pred_antilog <- exp(pdi_df_comb_sample_10_pred$mod.1_pred)
+
+
+p.mod <- p + geom_line(data = pdi_df_comb_sample_10_pred,
+                       aes(duration.mid, mod.1_pred_antilog,
+                           col = "Model",
+                           lty =  as.factor(type)),
+                       lwd = 1.5, alpha = 0.6, show.legend = FALSE,
+                       col = "black")
+p.mod <- p.mod + geom_point(data = pdi_df_comb_sample_10_pred,
+                       aes(duration.mid, mod.1_pred_antilog,
+                           col = "Model"),
+                       alpha = 0.8, show.legend = FALSE,
+                       col = "black", size=4)
+p.mod
+ggsave("pdi_plot_thing.pdf")
+# ?exp
+
+length(unique(p2))
+# ?glmer
+
+
+# mod.1 <- glmer(pdi_25_log ~ duration.mid + type + 1|ring_number,
+#                data = pdi_df_comb)
+# summary(mod.1)
