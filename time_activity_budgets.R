8fafe3585a5b2f3d4fad8270235c183bd489d12f
Tom Evans <thomas.jude.evans@gmail.com>
2016-07-04 10:40:18 +0200

    Exploring making time-activity budgets from the TDR records

---
+++ time_activity_budgets.R
@@ -0,0 +1,29 @@
+# Constructing activity budgets from TDR data (first using GPS data to vallidate)
+
+
+# Load in require data ----
+
+# GPS all
+
+# For GPS locations calculate distance from colony for each
+
+
+
+# TDR all
+
+# Deployment details
+# - Including start and end times
+
+# For one GPS + TDR deployment plot data together ----
+
+# Get start time for deployment
+
+# Get end time for deployment
+
+
+
+# Make a multi-panel figure with 5 rows
+
+par(mfrow = c(5,1))
+
+
