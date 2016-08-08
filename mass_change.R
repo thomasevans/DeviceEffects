c4ebdaa8960258b9d3e4373bf523009dbb3edea5
thomasevans <thomas.jude.evans@gmail.com>
2016-08-08 12:12:10 +0200

    Mass changes - pseudocode

---
+++ mass_change.R
@@ -0,0 +1,34 @@
+
+
+# Read in raw data ----
+
+
+# Make calculations ----
+
+# day of June
+
+# Residual mass (following seasonal correction)
+
+# Change in mass/ Cort
+# From last event
+
+# From start
+
+# Above per day
+
+# Make new data-frame ----
+
+# Save to RData file
+
+# Save to csv file
+
+
+# Make ggplot figures for mass changes -----
+
+# - Actual mass ----
+
+
+# - Residual mass -----
+
+
+# LMM models -----
