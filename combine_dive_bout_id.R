8fe940d44bed422a8ab26768b6f5fabff625aedd
thomasevans <thomas.jude.evans@gmail.com>
2016-08-10 20:19:49 +0200

    Script to combine some information from the dive bout and individual dive info DB tables

---
+++ combine_dive_bout_id.R
@@ -0,0 +1,16 @@
+
+# Read in dive data table
+
+
+# Read in bout data table
+
+
+# Get bout ID for each dive
+
+
+
+# Number dives for each bout
+
+# Plus label: first, middle, final
+
+# And get PDI from pause for next dive (for all but final dive)
