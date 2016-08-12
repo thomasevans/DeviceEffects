038b7eb4ea52d2ea3b928633c2787d1ba7c40b5b
thomasevans <thomas.jude.evans@gmail.com>
2016-08-12 15:56:56 +0200

    Various figures and analysis code

--- dive_depth.R
+++ dive_depth.R
@@ -4,65 +4,486 @@
 # Read in diving data
 
 
+# DB package
+library("RODBC")
+
+# Connect to DB
+gps.db <- odbcConnectAccess2007('D:/Dropbox/tracking_db/murre_db/murre_db.accdb')
+
+
+# Load in dive data
+dives <- sqlQuery(gps.db,
+                  query = "SELECT guillemots_GPS_TDR_dives.*, guillemots_GPS_TDR_dives_details.dive_bout_id, guillemots_GPS_TDR_dives_details.divetype, guillemots_GPS_TDR_dives_details.ndive, guillemots_GPS_TDR_dives_details.pdi, guillemots_GPS_TDR_dives_details.sun_elevation, guillemots_GPS_TDR_dives_details.day_period
+FROM guillemots_GPS_TDR_dives INNER JOIN guillemots_GPS_TDR_dives_details ON guillemots_GPS_TDR_dives.dive_id = guillemots_GPS_TDR_dives_details.dive_id
+ORDER BY guillemots_GPS_TDR_dives.TDR_deployment_id, guillemots_GPS_TDR_dives.date_time;
+",
+                  as.is = TRUE)
+dives$date_time <- as.POSIXct(dives$date_time,
+                              tz = "UTC")
+
+
+
 # Read in deployment data
+# Load in deployment information
+# (need to label by device order and device type deployed)
+tdr.deployments <- sqlQuery(gps.db,
+                            query = "SELECT guillemots_GPS_TDR_deployments_TDR.TDR_deployment_id, guillemots_GPS_TDR_deployments_TDR.TDR_ID, guillemots_GPS_TDR_events.GPS_TDR_event_id, guillemots_GPS_TDR_events.date_time_rel_utc, guillemots_GPS_TDR_events.ring_number, guillemots_GPS_TDR_events.GPS_TDR_event, guillemots_GPS_TDR_events.GPS_TDR_order
+                            FROM guillemots_GPS_TDR_deployments_TDR INNER JOIN guillemots_GPS_TDR_events ON guillemots_GPS_TDR_deployments_TDR.GPS_TDR_event_id_ON = guillemots_GPS_TDR_events.GPS_TDR_event_id;
+                            ",
+                            as.is = TRUE)
 
 
 # Combine above two
 # I.e. so can see if with GPS+TDR or TDR alone + deployment order
+# Add deployment info to pdi df (GPS/ TDR and order of deployment) ----
+tdr.deployments$type <- NA
+tdr.deployments$type[tdr.deployments$GPS_TDR_order == "GPS_first" & tdr.deployments$GPS_TDR_event == 1] <- "GPS"
+tdr.deployments$type[tdr.deployments$GPS_TDR_order == "GPS_first" & tdr.deployments$GPS_TDR_event == 2] <- "TDR"
+tdr.deployments$type[tdr.deployments$GPS_TDR_order == "TDR_first" & tdr.deployments$GPS_TDR_event == 1] <- "TDR"
+tdr.deployments$type[tdr.deployments$GPS_TDR_order == "TDR_first" & tdr.deployments$GPS_TDR_event == 2] <- "GPS"
 
 
 
-# Taking time of day into account - thinking of light level ----
-# Using sun elevation function from GeoLight package
-library("GeoLight")
+dives_df <- merge(dives, tdr.deployments,
+                     by = "TDR_deployment_id")
 
-# Get sun elevation angle for given date_time (POSIXct format)
-fun_sun_elev <- function(t){
-  require(GeoLight)
-  
-  # Solar time
-  s <- solar(t)
-  
-  # Island centre
-  lon <- 17.972
-  lat <- 57.285
-  
-  # Zenith angle (angle below vertical)
-  z <- zenith(s,lon,lat)
-  
-  # Calculate sun elevation (angle above horizon, negative if below)
-  h <- -1*(z-90)
-  
-  return(h)
+# Remove record for murre where both sets of data were not collected
+dives_df <- dives_df[dives_df$TDR_deployment_id != 1,]
+
+
+# Day of june -----
+# day of June
+dives_df$june_day <- as.numeric(format(dives_df$date_time, "%d"))
+hist(dives_df$june_day)
+
+
+# Statistical models ----
+
+library(lme4)
+library(arm)
+library(lattice)
+library(MuMIn)
+library(pbkrtest)
+
+KRSumFun <- function(object, objectDrop, ...) {
+  krnames <- c("ndf","ddf","Fstat","p.value","F.scaling")
+  r <- if (missing(objectDrop)) {
+    setNames(rep(NA,length(krnames)),krnames)
+  } else {
+    krtest <- KRmodcomp(object,objectDrop)
+    unlist(krtest$stats[krnames])
+  }
+  attr(r,"method") <- c("Kenward-Roger via pbkrtest package")
+  r
 }
 
-# Test above
-  # Make a vector of times
-  times <- seq(as.POSIXct("2015-06-21 00:00", tz = "UTC"), by = "hour", length.out = 24)
-  
-  # Calculate elevations
-  elevs <- sapply(times, fun_sun_elev)  
-  
-  # See how this looks
-  plot(elevs~times, type = "b")
-  abline(v=times, lty = 2, col = "light grey")
+hist(dives_df$depth_max_m)
+hist(log10(dives_df$depth_max_m))
+hist(sqrt(dives_df$depth_max_m))
+# x<-dives[which(dives$depth_max_m>90),]
+names(dives_df)[names(dives_df)=="ring_number.x"] <- "ring_number"
+
+dives_df$depth_max_m_log10 <- log10(dives_df$depth_max_m)
+dives_df$depth_max_m_ln <- log(dives_df$depth_max_m)
+dives_df$depth_max_m_sqrt <- sqrt(dives_df$depth_max_m)
 
 
-# Label by day period
-# - day >0 deg
-# - civil twilight <0 & >-6
-# - night <-6
-# Alternatively could use nautical twilight, which is down to -12
+models <- list()
 
-# Function to label period of day based on sun elevation angle  
-fun_day_per <- function(h){
-  if(h< -6)return("NIGHT")else{
-    if(h < 0)return("TWLGHT")else return("DAY")
+
+models[[1]] <- glmer(depth_max_m_log10 ~
+                       GPS_TDR_order*type +
+                       june_day +
+                       day_period +
+                          (1|ring_number/june_day/dive_bout_id),
+                          data = dives_df)
+
+
+
+# 
+# mod.n <- glmer(depth_max_m ~
+#         GPS_TDR_order*type +
+#         june_day +
+#         day_period +
+#         (1|ring_number/june_day/dive_bout_id),
+#       data = dives_df)
+# 
+# mod.ln <- glmer(depth_max_m_ln ~
+#         GPS_TDR_order*type +
+#         june_day +
+#         day_period +
+#         (1|ring_number/june_day/dive_bout_id),
+#       data = dives_df)
+# 
+# mod.log10 <- glmer(depth_max_m_log10 ~
+#         GPS_TDR_order*type +
+#         june_day +
+#         day_period +
+#         (1|ring_number/june_day/dive_bout_id),
+#       data = dives_df)
+# 
+# mod.sqrt <- glmer(depth_max_m_sqrt ~
+#         GPS_TDR_order*type +
+#         june_day +
+#         day_period +
+#         (1|ring_number/june_day/dive_bout_id),
+#       data = dives_df)
+# 
+# r.squaredGLMM(mod.n)
+# r.squaredGLMM(mod.ln)
+# r.squaredGLMM(mod.log10)
+# r.squaredGLMM(mod.sqrt)
+
+
+
+models[[2]] <- glmer(depth_max_m_log10 ~
+                       GPS_TDR_order+type +
+                       june_day +
+                       day_period +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df)
+
+models[[3]] <- glmer(depth_max_m_log10 ~
+                       GPS_TDR_order +
+                       june_day +
+                       day_period +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df)
+
+models[[4]] <- glmer(depth_max_m_log10 ~
+                       type +
+                       june_day +
+                       day_period +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df)
+
+models[[5]] <- glmer(depth_max_m_log10 ~
+                       june_day +
+                       day_period +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df)
+
+models[[6]] <- glmer(depth_max_m_log10 ~
+                       1 +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df)
+
+
+
+# Summarise information from the models
+models.aicc <- sapply(models, AICc)
+models.aicc.dif <- models.aicc-min(models.aicc)
+models.r2m <- sapply(models, r.squaredGLMM)
+t(models.r2m)
+MuMIn::r.squaredGLMM
+models.fit.df <- cbind.data.frame(c(1:length(models)),models.aicc,
+                                       models.aicc.dif,
+                                       t(models.r2m))
+names(models.fit.df) <- c("mod", "AICc", "dAICc", "R2m", "R2c")
+
+
+# Significance for dropped terms
+drop1(models[[1]], test = "user", sumFun = KRSumFun)
+
+# Cite: Halekoh, U., and Højsgaard, S. (2014). A kenward-roger approximation and parametric bootstrap methods for tests in linear mixed models–the R package pbkrtest. Journal of Statistical Software 59, 1–30.
+
+# "The result could reported as a Kenward-Roger corrected test with F(1, 118.5) = 16.17, p = .0001024"
+# From: https://seriousstats.wordpress.com/tag/kenward-roger-approximation/
+
+
+# Confidence intervals + coeficients
+model_va_coef <- summary(models[[1]])$coef[, 1]
+model_va_ci <- confint(models[[1]], method="Wald")
+model_va_par_df <- cbind.data.frame(model_va_coef,model_va_ci[-c(1:4),])
+
+summary(models[[1]])
+# Check model behaviour
+plot(models[[1]])
+qqmath(models[[1]])
+#   
+# library(influence.ME)
+# infl <- influence(models[[1]], obs = TRUE)
+# # (cooks.distance(infl))>0.06
+# plot(infl, which = "cook")
+
+# ??qqmath
+
+hist(dives_df$depth_max_m)
+
+range(dives_df$depth_max_m)
+
+
+# means etc -----
+library(plyr)
+
+se <- function(x){
+  x <- x[!is.na(x)]
+  sqrt(var(x)/length(x))
+} 
+
+type.means <- ddply(dives_df, .(GPS_TDR_order, type),
+                      summarise,
+                      depth_mean = mean(depth_max_m, na.rm = TRUE),
+                      depth_SE = se(depth_max_m),
+                      depth_log_mean = mean(depth_max_m_log10, na.rm = TRUE),
+                      depth_log_SE = se(depth_max_m_log10)
+                      
+)
+type.means
+
+type.means.ind <- ddply(dives_df, .(GPS_TDR_order, type, ring_number),
+                    summarise,
+                    depth_mean = mean(depth_max_m, na.rm = TRUE),
+                    depth_SE = se(depth_max_m),
+                    depth_log_mean = mean(depth_max_m_log10, na.rm = TRUE),
+                    depth_log_SE = se(depth_max_m_log10)
+                    
+)
+type.means.ind
+
+
+# Plot above ------
+
+
+# Colour scheme from http://colorbrewer2.org/, 3-class Dark2 qualitative, print and colour blind freindly
+col_3 <- c("#1b9e77","#d95f02", "#7570b3", "grey60")
+
+library("ggplot2")
+library(scales)
+library(cowplot)
+
+
+theme_new <- theme_bw(base_size = 14, base_family = "serif") +
+  theme(legend.position = c(1, 1),
+        legend.justification = c(1, 1),
+        legend.key.size =   unit(2, "lines"),
+        legend.key = element_rect(colour =NA),
+        legend.text = element_text(size = 12),
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 14),
+        legend.text.align = 0,
+        legend.key.width = unit(3, "lines"),
+        legend.title = element_blank()
+  )
+# 
+# ggplot(type.means, aes(x=type, y=depth_log_mean,
+#                        group= GPS_TDR_order)) +
+#   geom_line() +
+#   # geom_errorbar(width=.1, aes(ymin=value-ci, ymax=value+ci), colour="red") +
+#   geom_errorbar(width=.1, aes(ymin=type.means$depth_log_mean-type.means$depth_log_SE,
+#                               ymax=type.means$depth_log_mean+type.means$depth_log_SE)) +
+#   geom_point(shape=21, size=3, fill="white") +
+#   ylim(min(type.means$depth_log_mean),max(type.means$depth_log_mean))
+# 
+# 
+# 
+# ggplot(dives_df,
+#        aes(depth_max_m_log10, type, group = GPS_TDR_order)) +
+#   geom_jitter(alpha = I(1/4), aes(colour = GPS_TDR_order)) 
+# 
+# 
+# ggplot(data=dives_df, aes(x= GPS_TDR_order, y=depth_max_m, fill=type)) + 
+#   geom_boxplot() +
+#   theme_new
+# 
+# ggplot(data=type.means, aes(x= type, y=depth_log_mean,
+#                             group = GPS_TDR_order, colour = GPS_TDR_order)) + 
+#   geom_errorbar(aes(ymin=depth_log_mean-depth_log_SE, ymax=depth_log_mean+depth_log_SE), width=.1) +
+#   geom_line() +
+#   geom_point() +
+#   scale_colour_manual(values=col_3[1:2]) +
+#   scale_fill_manual(values=col_3[1:2]) +
+#   theme_new
+
+type.means$ring_number <- c(1,1,2,2)
+
+ggplot(data=type.means.ind, aes(x= type, y=depth_mean,
+                            group = ring_number, colour = GPS_TDR_order)) + 
+  geom_errorbar(aes(ymin=depth_mean-depth_SE, ymax=depth_mean+depth_SE),
+                width=.1, alpha = 0.3) +
+  geom_line(alpha = 0.3) +
+  geom_point(alpha = 0.3) +
+  geom_errorbar(data = type.means, aes(ymin=depth_mean-depth_SE,
+                                       ymax=depth_mean+depth_SE),
+                width=.2, , alpha = 0.8, lwd = 1) +
+  geom_line(data = type.means, alpha = 0.8, lwd = 1) +
+  geom_point(data = type.means, alpha = 0.8)+
+  scale_colour_manual(values=col_3[1:2]) +
+  scale_fill_manual(values=col_3[1:2]) +
+  labs(list(x = "Device type",
+            y = "Dive depth (m)",
+            fill = "Order")) +
+  # ylim(c(0,50))+
+  theme_new +
+  theme(legend.position = "top")
+
+ggsave(filename = "change_depth_mean_se.svg", width = 4, height = 4,
+       units = "in")
+ggsave(filename = "change_depth_mean_se.png", width = 4, height = 4,
+       units = "in")
+ggsave(filename = "change_depth_mean_se.pdf", width = 4, height = 4,
+       units = "in")
+# 
+# ggplot(data=type.means.ind, aes(x= type, y=depth_log_mean,
+#                                 group = ring_number, colour = GPS_TDR_order)) + 
+#   geom_errorbar(aes(ymin=depth_log_mean-depth_log_SE, ymax=depth_log_mean+depth_log_SE), width=.1) +
+#   geom_line() +
+#   geom_point() +
+#   scale_colour_manual(values=col_3[1:2]) +
+#   scale_fill_manual(values=col_3[1:2]) +
+#   theme_new
+
+hist(dives_df$depth_max_m, breaks = 100)
+hist(log10(dives_df$depth_max_m), breaks = 100)
+abline(v=c(13,50), col = "red")
+
+# 3-types of dive plot -----
+dive_type_fun <- function(x){
+  if(x <13) return("s") else{
+    if(x<50) return("m") else return("d")
   }
 }
 
-# Test this
-sapply(elevs, fun_day_per)
+dives_df$dive_type <- sapply(dives_df$depth_max_m, dive_type_fun)
+summary(as.factor(dive_type))
 
-# Day of june -----
+ggplot(data=dives_df, aes(x=type, fill=dive_type)) + 
+  geom_bar() + facet_wrap(~ GPS_TDR_order + ring_number, nrow = 2) +
+  theme_new +
+  theme(legend.position = c(1,0.4))
+ggsave(filename = "p_types_of_dive.svg", width = 8, height = 5,
+       units = "in")
+ggsave(filename = "p_types_of_dive.png", width = 8, height = 5,
+       units = "in")
+ggsave(filename = "p_types_of_dive.pdf", width = 8, height = 5,
+       units = "in")
 
+
+m <- ggplot(dives_df, aes(x = depth_max_m))
+m <- m + geom_histogram(colour = "dark grey", fill = "grey", binwidth = 2)
+m <- m + facet_grid(type ~ GPS_TDR_order)
+m <- m + geom_vline(xintercept = c(13, 50),
+                    lty = 2, lwd = 1.5, alpha = 0.6, col = "red")
+m + theme_new +  labs(list(x = "Dive depth (m)"))
+ggsave(filename = "dive_depth_hist.svg", width = 6, height = 5,
+       units = "in")
+ggsave(filename = "dive_depth_hist.png", width = 6, height = 5,
+       units = "in")
+ggsave(filename = "dive_depth_hist.pdf", width = 6, height = 5,
+       units = "in")
+
+
+# Logistic models for dives type -------
+dives_df$dive_type_bool <- dives_df$dive_type
+# Ignore shallow dives
+dives_df$dive_type_bool[dives_df$dive_type_bool == "s"] <- NA
+# Change to binnary, deep - true, medium - false
+dives_df$dive_type_bool[dives_df$dive_type_bool == "d"] <- TRUE
+dives_df$dive_type_bool[dives_df$dive_type_bool == "m"] <- FALSE
+dives_df$dive_type_bool <- as.factor(dives_df$dive_type_bool)
+
+
+# Stats models
+
+# glmer(got_eps~(1|ring_number), family=binomial(link='logit'), data=trips)
+
+
+models_logist <- list()
+
+dives_df_NAN <- dives_df[!is.na(dives_df$dive_type_bool),]
+dives_df_NAN$idx <- 1:nrow(dives_df_NAN)
+
+models_logist[[1]] <- glmer(dive_type_bool ~
+                       GPS_TDR_order*type +
+                       june_day +
+                       day_period + (1|idx)+
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df_NAN, family=binomial(link='logit'))
+# summary(models_logist[[1]])
+# models_logist[[7]] <- glmer(dive_type_bool ~
+#                               GPS_TDR_order*type +
+#                               june_day +
+#                               day_period + (1|dive_bout_id) +
+#                               (1|ring_number/june_day),
+#                             data = dives_df_NAN, family=binomial(link='logit'))
+# summary(models_logist[[7]])
+
+models_logist[[2]] <- glmer(dive_type_bool ~
+                       GPS_TDR_order+type +
+                       june_day +
+                       day_period + (1|idx) +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df_NAN, family=binomial(link='logit'))
+
+models_logist[[3]] <- glmer(dive_type_bool ~
+                       GPS_TDR_order +
+                       june_day +
+                       day_period +(1|idx) +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df_NAN, family=binomial(link='logit'))
+
+models_logist[[4]] <- glmer(dive_type_bool ~
+                       type +
+                       june_day +
+                       day_period +(1|idx) +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df_NAN, family=binomial(link='logit'))
+
+models_logist[[5]] <- glmer(dive_type_bool ~
+                       june_day +
+                       day_period +(1|idx) +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df_NAN, family=binomial(link='logit'))
+
+models_logist[[6]] <- glmer(dive_type_bool ~
+                       1 +(1|idx) +
+                       (1|ring_number/june_day/dive_bout_id),
+                     data = dives_df_NAN, family=binomial(link='logit'))
+
+# Based on this: https://ecologyforacrowdedplanet.wordpress.com/2013/08/27/r-squared-in-mixed-models-the-easy-way/
+r2fun <- function(x){
+  Vf <- var(model.matrix(x) %*% fixef(x))
+  Vr <- sum(unlist(print(VarCorr(x),comp="Variance")))
+  vresid <- var(resid(x))
+  # Vdist <- pi^2 / 3
+  Vtotal <- Vf + Vr + vresid
+  R2m <- Vf / (Vtotal)
+  R2c <- (Vf + Vr) / (Vtotal)
+  return(c(R2m,R2c))
+}
+
+# Summarise information from the models_logist
+models_logist.aicc <- sapply(models_logist, AICc)
+models_logist.aicc.dif <- models_logist.aicc-min(models_logist.aicc)
+models_logist.r2m <- sapply(models_logist, r.squaredGLMM)
+t(models_logist.r2m)
+
+models_logist.fit.df <- cbind.data.frame(c(1:length(models_logist)),models_logist.aicc,
+                                  models_logist.aicc.dif,
+                                  t(models_logist.r2m))
+names(models_logist.fit.df) <- c("mod", "AICc", "dAICc", "R2m", "R2c")
+
+summary(models_logist[[1]])
+# r.squaredGLMM(models_logist[[1]])
+
+
+# Significance for dropped terms
+drop1(models_logist[[1]], test = "user", sumFun = KRSumFun)
+
+# Cite: Halekoh, U., and Højsgaard, S. (2014). A kenward-roger approximation and parametric bootstrap methods for tests in linear mixed models_logist–the R package pbkrtest. Journal of Statistical Software 59, 1–30.
+
+# "The result could reported as a Kenward-Roger corrected test with F(1, 118.5) = 16.17, p = .0001024"
+# From: https://seriousstats.wordpress.com/tag/kenward-roger-approximation/
+
+
+
+# Confidence intervals + coeficients
+model_va_coef <- summary(models_logist[[1]])$coef[, 1]
+model_va_ci <- confint(models_logist[[1]], method="Wald")
+model_va_par_df <- cbind.data.frame(model_va_coef,model_va_ci[-c(1:4),])
+
+summary(models_logist[[1]])
+# Check model behaviour
+plot(models_logist[[1]])
+qqmath(models_logist[[1]])
+plot(models[[1]])
