2f94cc542c7c2d00ae38ad3f1229c059e4f757fb
thomasevans <thomas.jude.evans@gmail.com>
2016-08-18 21:19:29 +0200

    Proportion of activity in day/night

--- activity_budget_diving.R
+++ activity_budget_diving.R
@@ -93,6 +93,34 @@ for(i in 1:length(dep_id)){
   
 # For each full day calculate: ------
 
+#sunrise/set times
+library("maptools")
+# Island centre
+lon <- 17.972
+lat <- 57.285
+coord <- matrix(c(lon, lat), nrow = 1)
+sunrise <- sunriset(coord, dives$date_time, direction = "sunrise",
+                    POSIXct.out = TRUE)[,2]
+sunset <- sunriset(coord, dives$date_time, direction = "sunset",
+                   POSIXct.out = TRUE)[,2]
+td_90 <- as.difftime(90, units = "mins")
+sr_90 <- sunrise + td_90
+ss_90 <- sunset - td_90
+
+fun_time <- function(sun_r, sun_s, t){
+  if(t<sun_r) x <- "NIGHT" else{
+    if(t<sun_s) x <- "DAY" else{
+      x <- "NIGHT"
+    }
+  }
+  return(x)
+}
+
+dives$time_of_day <- mapply(fun_time, sun_r = sr_90, sun_s = ss_90, t = dives$date_time)
+summary(as.factor(dives$time_of_day))
+
+plot(dives$sun_elevation~dives$date_time, col = as.factor(dives$time_of_day))
+
 # * p(diving) etc -----
 library(plyr)
 # dives$duration_s
@@ -103,7 +131,28 @@ summary.df <- ddply(dives, .(TDR_deployment_id, june_day),
                     pdi_time_total = sum(pdi[!(divetype %in% c("final", "single")) &
                                                pdi <250]),
                     pdi_time_p = pdi_time_total/(60*60*24),
-                    dive_pdi_p = dive_time_p + pdi_time_p
+                    dive_pdi_p = dive_time_p + pdi_time_p,
+                    
+                    # Time of day
+                    dive_time_total_day = sum(duration_s[time_of_day == "DAY"],
+                                              na.rm = TRUE),
+                    dive_time_total_night = sum(duration_s[time_of_day == "NIGHT"],
+                                                na.rm = TRUE),
+                    pdi_time_day = sum(pdi[time_of_day == "DAY" &
+                                             !(divetype %in% c("final", "single")) &
+                                               pdi <250],
+                                       na.rm = TRUE),
+                    pdi_time_night = sum(pdi[time_of_day == "NIGHT" &
+                                               !(divetype %in% c("final", "single")) &
+                                               pdi <250],
+                                         na.rm = TRUE),
+                    dive_day_p = dive_time_total_day / (60*60*24),
+                    dive_night_p = dive_time_total_night / (60*60*24),
+                    pdi_day_p = pdi_time_day / (60*60*24),
+                    pdi_night_p = pdi_time_night / (60*60*24),
+                    dive_pdi_p_day = ((dive_day_p + pdi_day_p)/
+                      (dive_night_p + pdi_night_p + dive_day_p + pdi_day_p))
+                    
 )
 
 day.df.comb <- merge(day.df, summary.df, by = c("TDR_deployment_id", "june_day"))
@@ -122,6 +171,7 @@ day.df.comb$type[day.df.comb$GPS_TDR_order == "TDR_first" & day.df.comb$GPS_TDR_
 
 boxplot(day.df.comb$dive_pdi_p~day.df.comb$type+ day.df.comb$GPS_TDR_order)
 
+boxplot(day.df.comb$dive_pdi_p_day~day.df.comb$type+ day.df.comb$GPS_TDR_order)
 
 
 # Make some plots -----
@@ -167,6 +217,34 @@ ggsave(filename = "activty_dive_pdi_boxplot_no_legend.png", width = 4, height = 
 ggsave(filename = "activty_dive_pdi_boxplot_no_legend.pdf", width = 4, height = 4,
        units = "in")
 
+
+
+ggplot(day.df.comb,
+       aes(x = type, fill = GPS_TDR_order, y = dive_pdi_p_day*100))+
+  geom_boxplot(outlier.size = 0, alpha = 0.7) +
+  geom_point(pch = 21, position = position_jitterdodge(),
+             alpha = 0.6)+
+  labs(list(x = "Group",
+            y = "% daytime diving + PDI",
+            fill = "")) +
+  theme_new +
+  scale_colour_manual(values=col_3[1:3]) +
+  scale_fill_manual(values=col_3[1:3]) +
+  # theme(legend.position = c(0.4, 1)) +
+  theme(legend.position = "none")
+
+
+ggsave(filename = "activty_dive_pdi_boxplot_no_legend_day.svg", width = 4, height = 4,
+       units = "in")
+ggsave(filename = "activty_dive_pdi_boxplot_no_legend_day.png", width = 4, height = 4,
+       units = "in")
+ggsave(filename = "activty_dive_pdi_boxplot_no_legend_day.pdf", width = 4, height = 4,
+       units = "in")
+
+
+
+
+
 hist(day.df.comb$dive_pdi_p, breaks = 10)
 
 
@@ -202,6 +280,39 @@ ggsave(filename = "activty_dive_pdi_date.png", width = 6, height = 4,
 ggsave(filename = "activty_dive_pdi_date.pdf", width = 6, height = 4,
        units = "in")
 
+
+
+
+# Propotion day active
+
+p <- ggplot(day.df.comb, aes(june_day, dive_pdi_p_day,
+                             colour = factor(GPS_TDR_order),
+                             shape = type)) +
+  scale_colour_manual(values=col_3) +
+  geom_line(alpha = 0.6,
+            lwd = 0.8,
+            aes(group = factor(ring_number.x),
+                lty = factor(ring_number.x)),
+            # shape = NA,
+            show.legend = FALSE) +
+  geom_point(
+    alpha=0.8,
+    size=3,
+    show.legend =TRUE) +
+  theme_bw()  + guides(group=FALSE, lty=FALSE) 
+# p <- p  + scale_colour_manual(values=col_3)
+p <- p  + labs(list(x = "Date (day of June)", y =  "P(day active)", shape = "Device", col = "Order", fill = "Order"))
+p <- p + theme(legend.key.width=unit(2,"line"))
+p +scale_y_continuous(labels = percent,
+                      breaks = seq(0,1, .1),
+                      limits = c(0,1))
+# ?scale_y_continuous
+ggsave(filename = "activty_dive_pdi_date_day_P.svg", width = 6, height = 4,
+       units = "in")
+ggsave(filename = "activty_dive_pdi_date_day_P.png", width = 6, height = 4,
+       units = "in")
+ggsave(filename = "activty_dive_pdi_date_day_P.pdf", width = 6, height = 4,
+       units = "in")
 
 
 p <- ggplot(day.df.comb, aes(june_day, dive_time_p*100,
