4b8f92dc0aa0e9a7e25c689574670bac594d96a5
thomasevans <thomas.jude.evans@gmail.com>
2016-08-23 08:18:21 +0200

    Extract SST for dive bouts

---
+++ bouts_sst_extract.R
@@ -0,0 +1,16 @@
+# Bout + SST
+
+
+# Read in bout data ------
+
+
+
+# Get raw tag data ------
+
+
+
+# For each bout get summary stats for SST -----
+
+
+
+# Output these details to DB table ------
